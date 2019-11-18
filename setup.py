cfbdd286346054edfeb9934f2aa86ba8dfed87f1
haron1100 <hshams@hotmail.co.uk>
2019-11-18 22:24:46 +0000

    hopefully finally fixed

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open("README.md", 'r') as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
     
 setup(
@@ -8,16 +8,19 @@ setup(
     version = '0.0.1',
     description = 'Grid Environment for testing and teaching RL algorithms. Integrated into OpenAI Gym.',   # Give a short description about your library
     long_description = long_description,
-    long_description_content_type = 'text/markdown'
+    long_description_content_type='text/markdown',
     py_modules=["__init__"],
     package_dir={'':'GriddyEnv'},
+    author = 'Haron Shams',                   # Type in your name
+    author_email = 'hshams@hotmail.co.uk',      # Type in your E-Mail
+    url = 'https://github.com/haron1100/Griddy-Env',   # Provide either the link to your github or to your website
     classifiers=[
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Operating System :: OS Independent',
   ],
 )
 
@@ -29,9 +32,6 @@ setup(
   version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Grid Environment for testing and teaching RL algorithms. Integrated into OpenAI Gym.',   # Give a short description about your library
-  author = 'Haron Shams',                   # Type in your name
-  author_email = 'hshams@hotmail.co.uk',      # Type in your E-Mail
-  url = 'https://github.com/haron1100/Griddy-Env',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/haron1100/Griddy-Env/archive/v0.0.1.tar.gz',    # I explain this later on
   keywords = ['Reinforcement Learning', 'AI', 'ML', 'Machine Learning', 'Artificial Intelligence', 'Simulation'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
